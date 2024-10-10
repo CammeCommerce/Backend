084f997aa6f5d2cf161137f50a4a4d29cd75c521
희은 <datakubit2@gmail.com>
2024-10-10 13:57:53 +0900

    feat: Order 도메인 개발 환경 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from "src/app.service";
 import { UserModule } from "src/domain/user/user.module";
 import { MediumModule } from "src/domain/medium/medium.module";
 import { SettlementCompanyModule } from "src/domain/settlement-company/settlement-company.module";
+import { OrderModule } from "src/domain/order/order.module";
 
 @Module({
-  imports: [UserModule, MediumModule, SettlementCompanyModule],
+  imports: [UserModule, MediumModule, SettlementCompanyModule, OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/domain/order/order.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Order } from "src/domain/order/entity/order.entity";
+import { OrderController } from "src/domain/order/controller/order.controller";
+import { OrderService } from "src/domain/order/service/order.service";
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Order])],
+  controllers: [OrderController],
+  providers: [OrderService],
+})
+export class OrderModule {}
