c56d69795561f35a6c8e6f280a36763150abd925
희은 <datakubit2@gmail.com>
2024-10-11 01:58:20 +0900

    fix: 매체명 수정 결과 DTO description 변경

--- src/domain/medium/dto/response/modify-medium-result.dto.ts
+++ src/domain/medium/dto/response/modify-medium-result.dto.ts
@@ -4,7 +4,7 @@ export class ModifyMediumResultDto {
   @ApiProperty({ example: 1, description: "매체 ID" })
   id: number;
 
-  @ApiProperty({ example: "포앤서치", description: "매체명" })
+  @ApiProperty({ example: "포앤서치", description: "수정된 매체명" })
   name: string;
 
   @ApiProperty({ example: "2024-10-10", description: "수정일자" })
