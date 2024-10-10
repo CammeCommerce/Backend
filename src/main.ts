646e5a40972acc4659fab3b48427a961b50392f3
희은 <datakubit2@gmail.com>
2024-10-10 17:28:24 +0900

    feat: Swagger를 사용한 API 문서화 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle("API Documentation")
+    .setVersion("1.0")
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup("api", app, swaggerDocument);
+
   await app.listen(8080);
 }
 bootstrap();
