1ffb766e6cfefc8272f76d73ff9dd4fdbdd2db46
희은 <datakubit2@gmail.com>
2024-10-11 19:58:46 +0900

    feat: CORS 허용 및 시간 관련 의존성 설정

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { NestExpressApplication } from "@nestjs/platform-express";
+import * as moment from "moment-timezone";
 
 async function bootstrap() {
+  moment.tz.setDefault("Asia/Seoul");
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle("API Documentation")
