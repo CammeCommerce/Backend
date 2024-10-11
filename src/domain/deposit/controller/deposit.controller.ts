531ad4e35c4cd3be653e922d1f54601c5ed44d3d
희은 <datakubit2@gmail.com>
2024-10-11 21:47:02 +0900

    feat: 입금값 조회/수정/삭제 API 구현

--- src/domain/deposit/controller/deposit.controller.ts
+++ src/domain/deposit/controller/deposit.controller.ts
@@ -1,8 +1,21 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from "@nestjs/common";
 import { DepositService } from "src/domain/deposit/service/deposit.service";
 import { ApiOperation } from "@nestjs/swagger";
 import { CreateDepositDto } from "src/domain/deposit/dto/request/create-deposit.dto";
 import { CreateDepositResultDto } from "src/domain/deposit/dto/response/create-deposit-result.dto";
+import { GetDepositsDto } from "src/domain/deposit/dto/response/get-deposit.dto";
+import { ModifyDepositDto } from "src/domain/deposit/dto/request/modify-deposit.dto";
+import { ModifyDepositResultDto } from "src/domain/deposit/dto/response/modify-deposit-result.dto";
 
 @Controller("deposit")
 export class DepositController {
@@ -18,5 +31,39 @@ export class DepositController {
     @Body() dto: CreateDepositDto
   ): Promise<CreateDepositResultDto> {
     return await this.depositService.createDeposit(dto);
+  }
+
+  @ApiOperation({
+    summary: "입금값 조회",
+    operationId: "getDeposits",
+    tags: ["deposit"],
+  })
+  @Get()
+  async getDeposits(): Promise<GetDepositsDto> {
+    return await this.depositService.getDeposits();
+  }
+
+  @ApiOperation({
+    summary: "입금값 수정",
+    operationId: "modifyDeposit",
+    tags: ["deposit"],
+  })
+  @Patch(":id")
+  async modifyDeposit(
+    @Param("id", ParseIntPipe) id: number,
+    @Body() dto: ModifyDepositDto
+  ): Promise<ModifyDepositResultDto> {
+    return await this.depositService.modifyDeposit(id, dto);
+  }
+
+  @ApiOperation({
+    summary: "입금값 삭제",
+    operationId: "deleteDeposit",
+    tags: ["deposit"],
+  })
+  @Delete(":id")
+  @HttpCode(204)
+  async deleteDeposit(@Param("id", ParseIntPipe) id: number): Promise<void> {
+    return await this.depositService.deleteDeposit(id);
   }
 }
--- src/domain/deposit/service/deposit.service.ts
+++ src/domain/deposit/service/deposit.service.ts
@@ -1,10 +1,16 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Deposit } from "src/domain/deposit/entity/deposit.entity";
 import { Repository } from "typeorm";
 import { CreateDepositDto } from "src/domain/deposit/dto/request/create-deposit.dto";
 import { CreateDepositResultDto } from "src/domain/deposit/dto/response/create-deposit-result.dto";
 import { plainToInstance } from "class-transformer";
+import {
+  DepositDetailDto,
+  GetDepositsDto,
+} from "src/domain/deposit/dto/response/get-deposit.dto";
+import { ModifyDepositDto } from "src/domain/deposit/dto/request/modify-deposit.dto";
+import { ModifyDepositResultDto } from "src/domain/deposit/dto/response/modify-deposit-result.dto";
 
 @Injectable()
 export class DepositService {
@@ -18,7 +24,7 @@ export class DepositService {
     const deposit = await this.depositRepository.create(dto);
     await this.depositRepository.save(deposit);
 
-    // 입금명 등록 결과 DTO 생성
+    // 입금값 등록 결과 DTO 생성
     const createDepositResultDto = plainToInstance(CreateDepositResultDto, {
       id: deposit.id,
       mediumName: deposit.mediumName,
@@ -35,5 +41,105 @@ export class DepositService {
     });
 
     return createDepositResultDto;
+  }
+
+  // 입금값 조회
+  async getDeposits(): Promise<GetDepositsDto> {
+    const deposits = await this.depositRepository.find({
+      where: { isDeleted: false },
+      order: { createdAt: "ASC" },
+    });
+
+    if (!deposits) {
+      throw new NotFoundException("등록된 입금값이 없습니다.");
+    }
+
+    const items = deposits.map((deposit) =>
+      plainToInstance(DepositDetailDto, {
+        id: deposit.id,
+        mediumName: deposit.mediumName,
+        depositDate: deposit.depositDate,
+        accountAlias: deposit.accountAlias,
+        depositAmount: deposit.depositAmount,
+        accountDescription: deposit.accountDescription,
+        transactionMethod1: deposit.transactionMethod1,
+        transactionMethod2: deposit.transactionMethod2,
+        accountMemo: deposit.accountMemo,
+        counterpartyName: deposit.counterpartyName,
+        purpose: deposit.purpose,
+        clientName: deposit.clientName,
+      })
+    );
+
+    // 입금 배열 DTO 생성
+    const depositItemsDto = new GetDepositsDto();
+    depositItemsDto.items = items;
+
+    return depositItemsDto;
+  }
+
+  // 입금값 수정
+  async modifyDeposit(
+    id: number,
+    dto: ModifyDepositDto
+  ): Promise<ModifyDepositResultDto> {
+    const deposit = await this.depositRepository.findOne({
+      where: { id, isDeleted: false },
+    });
+
+    if (!deposit) {
+      throw new NotFoundException("입금값을 찾을 수 없습니다.");
+    }
+
+    deposit.mediumName = dto.mediumName;
+    deposit.depositDate = dto.depositDate;
+    deposit.accountAlias = dto.accountAlias;
+    deposit.depositAmount = dto.depositAmount;
+    deposit.accountDescription = dto.accountDescription;
+    deposit.transactionMethod1 = dto.transactionMethod1;
+    deposit.transactionMethod2 = dto.transactionMethod2;
+    deposit.accountMemo = dto.accountMemo;
+    deposit.counterpartyName = dto.counterpartyName;
+    deposit.purpose = dto.purpose;
+    deposit.clientName = dto.clientName;
+    deposit.updatedAt = new Date();
+
+    await this.depositRepository.save(deposit);
+
+    const modifyDepositResultDto = plainToInstance(ModifyDepositResultDto, {
+      id: deposit.id,
+      mediumName: deposit.mediumName,
+      depositDate: deposit.depositDate,
+      accountAlias: deposit.accountAlias,
+      depositAmount: deposit.depositAmount,
+      accountDescription: deposit.accountDescription,
+      transactionMethod1: deposit.transactionMethod1,
+      transactionMethod2: deposit.transactionMethod2,
+      accountMemo: deposit.accountMemo,
+      counterpartyName: deposit.counterpartyName,
+      purpose: deposit.purpose,
+      clientName: deposit.clientName,
+      updatedAt: deposit.updatedAt,
+    });
+
+    return modifyDepositResultDto;
+  }
+
+  // 입금값 삭제
+  async deleteDeposit(id: number): Promise<void> {
+    const deposit = await this.depositRepository.findOne({
+      where: { id, isDeleted: false },
+    });
+
+    if (!deposit) {
+      throw new NotFoundException("입금값을 찾을 수 없습니다.");
+    }
+
+    deposit.deletedAt = new Date();
+    deposit.isDeleted = true;
+
+    await this.depositRepository.save(deposit);
+
+    return;
   }
 }
