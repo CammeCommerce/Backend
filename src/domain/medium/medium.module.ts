71211dcbdd3755399b55b0b23fc0610a2283af01
희은 <datakubit2@gmail.com>
2024-10-28 18:03:48 +0900

    fix: 매체 도메인에 의존성 주입

--- src/domain/medium/medium.module.ts
+++ src/domain/medium/medium.module.ts
@@ -4,9 +4,15 @@ import { MediumController } from "src/domain/medium/controller/medium.controller
 import { MediumService } from "src/domain/medium/service/medium.service";
 import { Medium } from "src/domain/medium/entity/medium.entity";
 import { AuthModule } from "src/domain/auth/auth.module";
+import { Order } from "src/domain/order/entity/order.entity";
+import { Deposit } from "src/domain/deposit/entity/deposit.entity";
+import { Withdrawal } from "src/domain/withdrawal/entity/withdrawal.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Medium]), AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([Medium, Order, Deposit, Withdrawal]),
+    AuthModule,
+  ],
   controllers: [MediumController],
   providers: [MediumService],
 })
