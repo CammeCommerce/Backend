8b6259a4e462008487d8b37319db41a28fdb95ce
희은 <datakubit2@gmail.com>
2024-12-01 15:07:39 +0900

    fix: 매칭 데이터 외의 필드 nullable 허용

--- src/domain/order/entity/order.entity.ts
+++ src/domain/order/entity/order.entity.ts
@@ -9,34 +9,34 @@ export class Order extends BaseEntity {
   @Column({ comment: "정산업체명", nullable: true })
   settlementCompanyName: string;
 
-  @Column({ comment: "상품명" })
+  @Column({ comment: "상품명", nullable: true })
   productName: string;
 
-  @Column({ comment: "수량" })
+  @Column({ comment: "수량", nullable: true })
   quantity: number;
 
-  @Column({ comment: "발주일자" })
+  @Column({ comment: "발주일자", nullable: true })
   orderDate: string;
 
-  @Column({ comment: "매입처" })
+  @Column({ comment: "매입처", nullable: true })
   purchasePlace: string;
 
-  @Column({ comment: "매출처" })
+  @Column({ comment: "매출처", nullable: true })
   salesPlace: string;
 
-  @Column({ comment: "매입가" })
+  @Column({ comment: "매입가", nullable: true })
   purchasePrice: number;
 
-  @Column({ comment: "판매가" })
+  @Column({ comment: "판매가", nullable: true })
   salesPrice: number;
 
-  @Column({ comment: "매입 배송비" })
+  @Column({ comment: "매입 배송비", nullable: true })
   purchaseShippingFee: number;
 
-  @Column({ comment: "매출 배송비" })
+  @Column({ comment: "매출 배송비", nullable: true })
   salesShippingFee: number;
 
-  @Column({ comment: "과세여부" })
+  @Column({ comment: "과세여부", nullable: true })
   taxType: string;
 
   @Column({ comment: "마진액" })
--- src/domain/withdrawal/entity/withdrawal.entity.ts
+++ src/domain/withdrawal/entity/withdrawal.entity.ts
@@ -6,31 +6,31 @@ export class Withdrawal extends BaseEntity {
   @Column({ comment: "매체명", nullable: true })
   mediumName: string;
 
-  @Column({ comment: "출금일자" })
+  @Column({ comment: "출금일자", nullable: true })
   withdrawalDate: string;
 
-  @Column({ comment: "계좌 별칭" })
+  @Column({ comment: "계좌 별칭", nullable: true })
   accountAlias: string;
 
-  @Column({ comment: "출금액" })
+  @Column({ comment: "출금액", nullable: true })
   withdrawalAmount: number;
 
-  @Column({ comment: "계좌 적요" })
+  @Column({ comment: "계좌 적요", nullable: true })
   accountDescription: string;
 
-  @Column({ comment: "거래수단1" })
+  @Column({ comment: "거래수단1", nullable: true })
   transactionMethod1: string;
 
-  @Column({ comment: "거래수단2" })
+  @Column({ comment: "거래수단2", nullable: true })
   transactionMethod2: string;
 
-  @Column({ comment: "계좌 메모" })
+  @Column({ comment: "계좌 메모", nullable: true })
   accountMemo: string;
 
-  @Column({ comment: "용도" })
+  @Column({ comment: "용도", nullable: true })
   purpose: string;
 
-  @Column({ comment: "거래처" })
+  @Column({ comment: "거래처", nullable: true })
   clientName: string;
 
   @Column({ comment: "매체명 매칭 여부", default: false })
